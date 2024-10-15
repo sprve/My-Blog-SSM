a9ef27f9c14531d629493e9a20ed9a523e39e235
sprve <sprveing@gmail.com>
2024-10-15 19:24:40 +0800

    Update

--- Blog-User/src/main/java/com/sprve/UserApplication.java
+++ Blog-User/src/main/java/com/sprve/UserApplication.java
@@ -1,6 +1,5 @@
 package com.sprve;
 
-import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
