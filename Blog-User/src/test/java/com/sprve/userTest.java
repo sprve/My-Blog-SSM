5db22011e7c923f257fc62d3adc681d17dc7cdeb
sprve <sprveing@gmail.com>
2024-11-28 11:45:26 +0800

    Update

--- Blog-User/src/test/java/com/sprve/userTest.java
+++ Blog-User/src/test/java/com/sprve/userTest.java
@@ -1,8 +1,7 @@
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+package com.sprve;
+
 import com.github.yulichang.query.MPJLambdaQueryWrapper;
-import com.sprve.UserApplication;
 import com.sprve.domain.entity.Article;
-import com.sprve.domain.entity.Category;
 import com.sprve.mapper.ArticleMapper;
 import com.sprve.mapper.CategoryMapper;
 import com.sprve.service.ArticleService;
@@ -10,11 +9,16 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @SpringBootTest(classes= UserApplication.class)
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @Slf4j
 public class userTest {
 
@@ -30,6 +34,12 @@ public class userTest {
     @Test
     public void list(){
         MPJLambdaQueryWrapper<Article> queryWrapper = new MPJLambdaQueryWrapper();
+        queryWrapper.selectAll(Article.class);
+        List<Article> articleList = articleMapper.selectJoinList(queryWrapper);
+        for(Article article : articleList){
+            log.info(article.getId()+" "+article.getCategoryName());
+        }
         log.info("测试中");
+
     }
 }
